d79c328c8a32561aa8ec3b618690dbf0fc665dda
Aashma457 <Aashmaadhikari9@gmail.com>
2024-10-01 17:05:24 +0545

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+// function sum(a,b)
+// {
+//    s=a+b;
+//    console.log(a)
+//    return(s)
+   
+// }
+// let v=sum(2,8);
+// console.log(v);
+
+
+// const sum=(a,b)=>{
+//     console.log(sum)
+// }
+// const multi=(a,b)=>{
+//     console.log(a*b)
+// }
+function countVowel(str)
+{
+   for(const char of str)
+   {
+      console.log(char);
+   }
+}
